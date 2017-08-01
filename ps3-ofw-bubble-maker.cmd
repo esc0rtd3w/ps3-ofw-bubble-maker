1788cd525a236581e18c76cda7e23b352b1ad0c5
Jason Borowski <esc0rtd3w@gmail.com>
2017-08-01 01:42:51 -0400

    Updated d0 and d1 template files

--- ps3-ofw-bubble-maker.cmd
+++ ps3-ofw-bubble-maker.cmd
@@ -29,7 +29,8 @@ set dOneName=d1.pdb
 set fZeroName=f0.pdb
 set iconFileName=ICON_FILE
 
-set dZeroChunkData=d0_chunk1.bin+d0_chunk2.bin+d0_chunk3.bin+d0_chunk4.bin+d0_chunk5.bin
+set dZeroChunkData=d0_chunk1.bin+d0_chunk2.bin+d0_chunk3.bin
+set dOneChunkData=d1_chunk1.bin+d1_chunk2.bin+d1_chunk3.bin+d1_chunk4.bin+d1_chunk5.bin
 
 set root=%~dp0
 set pathOutput=%root%\output\vsh
@@ -45,12 +46,12 @@ set dZeroTemplate=%pathTemplateGamePKG%\%pkgNumberBase%\d0.pdb
 set dZeroChunkOne=%pathTemplateGamePKG%\%pkgNumberBase%\d0_chunk1.bin
 set dZeroChunkTwo=%pathTemplateGamePKG%\%pkgNumberBase%\d0_chunk2.bin
 set dZeroChunkThree=%pathTemplateGamePKG%\%pkgNumberBase%\d0_chunk3.bin
-set dZeroChunkFour=%pathTemplateGamePKG%\%pkgNumberBase%\d0_chunk4.bin
-set dZeroChunkFive=%pathTemplateGamePKG%\%pkgNumberBase%\d0_chunk5.bin
-set dOneChunkZero=%pathTemplateGamePKG%\%pkgNumberBase%\d1_chunk0.bin
 set dOneChunkOne=%pathTemplateGamePKG%\%pkgNumberBase%\d1_chunk1.bin
 set dOneChunkTwo=%pathTemplateGamePKG%\%pkgNumberBase%\d1_chunk2.bin
 set dOneChunkThree=%pathTemplateGamePKG%\%pkgNumberBase%\d1_chunk3.bin
+set dOneChunkFour=%pathTemplateGamePKG%\%pkgNumberBase%\d1_chunk4.bin
+set dOneChunkFourDefault=%pathTemplateGamePKG%\%pkgNumberBase%\d1_chunk4_default.bin
+set dOneChunkFive=%pathTemplateGamePKG%\%pkgNumberBase%\d1_chunk5.bin
 set dOneTemplate=%pathTemplateGamePKG%\%pkgNumberBase%\d1.pdb
 set fZeroTemplate=%pathTemplateGamePKG%\%pkgNumberBase%\f0.pdb
 set iconFileTemplate=%pathTemplateGamePKG%\%pkgNumberBase%\ICON_FILE
@@ -129,66 +130,6 @@ setlocal enabledelayedexpansion
 set /p charsTotalTemp=<%temp%\charsTotal.txt
 set /a charsPaddingTemp=47-%charsTotalTemp%
 
-:: Some Super Hacky Padding
-for %%a in (%charsPaddingTemp%) do (
-    for /f "tokens=*" %%F in ("%%a") do (
-	   set /a num=%%F
-       set spaces=
-       if !num! equ 47 set spaces=.
-       if !num! equ 46 set spaces= .
-       if !num! equ 45 set spaces=  .
-       if !num! equ 44 set spaces=   .
-       if !num! equ 43 set spaces=    .
-       if !num! equ 42 set spaces=     .
-       if !num! equ 41 set spaces=      .
-       if !num! equ 40 set spaces=       .
-       if !num! equ 39 set spaces=        .
-       if !num! equ 38 set spaces=         .
-       if !num! equ 37 set spaces=          .
-       if !num! equ 36 set spaces=           .
-       if !num! equ 35 set spaces=            .
-       if !num! equ 34 set spaces=             .
-       if !num! equ 33 set spaces=              .
-       if !num! equ 32 set spaces=               .
-       if !num! equ 31 set spaces=                .
-       if !num! equ 30 set spaces=                 .
-       if !num! equ 29 set spaces=                  .
-       if !num! equ 28 set spaces=                   .
-       if !num! equ 27 set spaces=                    .
-       if !num! equ 26 set spaces=                     .
-       if !num! equ 25 set spaces=                      .
-       if !num! equ 24 set spaces=                       .
-       if !num! equ 23 set spaces=                        .
-       if !num! equ 22 set spaces=                         .
-       if !num! equ 21 set spaces=                          .
-       if !num! equ 20 set spaces=                           .
-       if !num! equ 19 set spaces=                            .
-       if !num! equ 18 set spaces=                             .
-       if !num! equ 17 set spaces=                              .
-       if !num! equ 16 set spaces=                               .
-       if !num! equ 15 set spaces=                                .
-       if !num! equ 14 set spaces=                                 .
-       if !num! equ 13 set spaces=                                  .
-       if !num! equ 12 set spaces=                                   .
-       if !num! equ 11 set spaces=                                    .
-       if !num! equ 10 set spaces=                                     .
-       if !num! equ 9 set spaces=                                       .
-       if !num! equ 8 set spaces=                                        .
-       if !num! equ 7 set spaces=                                         .
-       if !num! equ 6 set spaces=                                          .
-       if !num! equ 5 set spaces=                                           .
-       if !num! equ 4 set spaces=                                            .
-       if !num! equ 3 set spaces=                                             .
-       if !num! equ 2 set spaces=                                              .
-       if !num! equ 1 set spaces=                                               .
-       if !num! equ 0 set spaces=                                                .
-       set "padding=!spaces!"
-       echo !padding!>"%temp%\installTextInputPadding.txt"
-    )
-)
-
-endlocal
-
 set /p paddingData=<%temp%\installTextInputPadding.txt
 
 
@@ -217,25 +158,27 @@ echo %paddingData%>"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2b.bin"
 :: Put Two Chunks Back Together
 copy /y "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2a.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2b.bin" "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2.bin"
 
-echo %pkgNumberBase%>"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk4.bin"
+:: The d1 value is the same as d0
+copy /y "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2.bin" "%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk2.bin"
+
+echo %pkgNumberBase%>"%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk4.bin"
 
 :: Build d0.pdb and d1.pdb
 xcopy /y "%dZeroChunkOne%" "%pathOutput%\game_pkg\%pkgNumberBase%\*"
 ::xcopy /y "%dZeroChunkTwo%" "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2.bin"
 xcopy /y "%dZeroChunkThree%" "%pathOutput%\game_pkg\%pkgNumberBase%\*"
-::xcopy /y "%dZeroChunkFour%" "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk4.bin"
-xcopy /y "%dZeroChunkFive%" "%pathOutput%\game_pkg\%pkgNumberBase%\*"
 xcopy /y "%fZeroTemplate%" "%pathOutput%\game_pkg\%pkgNumberBase%\*"
 
-xcopy /y "%dOneChunkZero%" "%pathOutput%\game_pkg\%pkgNumberBase%\*"
 xcopy /y "%dOneChunkOne%" "%pathOutput%\game_pkg\%pkgNumberBase%\*"
-xcopy /y "%dOneChunkTwo%" "%pathOutput%\game_pkg\%pkgNumberBase%\*"
+::xcopy /y "%dOneChunkTwo%" "%pathOutput%\game_pkg\%pkgNumberBase%\*"
 xcopy /y "%dOneChunkThree%" "%pathOutput%\game_pkg\%pkgNumberBase%\*"
+::xcopy /y "%dOneChunkFour%" "%pathOutput%\game_pkg\%pkgNumberBase%\*"
+xcopy /y "%dOneChunkFive%" "%pathOutput%\game_pkg\%pkgNumberBase%\*"
 
 
-copy /y "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk1.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk3.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk4.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk5.bin" "%pathOutput%\game_pkg\%pkgNumberBase%\d0.pdb"
+copy /y "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk1.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk3.bin" "%pathOutput%\game_pkg\%pkgNumberBase%\d0.pdb"
 
-copy /y "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk1.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk0.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk4.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk5.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk1.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk2.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk3.bin" "%pathOutput%\game_pkg\%pkgNumberBase%\d1.pdb"
+copy /y "%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk1.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk2.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk3.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk4.bin"+"%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk5.bin" "%pathOutput%\game_pkg\%pkgNumberBase%\d1.pdb"
 
 
 
@@ -262,12 +205,12 @@ del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2.bin"
 del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2a.bin"
 del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk2b.bin"
 del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk3.bin"
-del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk4.bin"
-del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d0_chunk5.bin"
-del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk0.bin"
 del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk1.bin"
 del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk2.bin"
 del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk3.bin"
+del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk4.bin"
+del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk4_default.bin"
+del /f /q "%pathOutput%\game_pkg\%pkgNumberBase%\d1_chunk5.bin"
 
 
 ::set pathRemote=%pathRemote%/%pkgNumberBase%
